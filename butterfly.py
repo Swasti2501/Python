60eac3f6597ffa97ae035bb19049ada2b2268161
Swasti Aggarwal <swastiaggarwal2501@gmail.com>
2025-10-21 17:20:41 +0530

    Print the butterfly pattern.

---
+++ butterfly.py
@@ -0,0 +1,21 @@
+n = int(input("Enter the number: "))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("* ", end=" ")
+    for j in range(1,n-i+1):
+        print("  ", end=" ")
+    for j in reversed(range(1,n-i+1)):
+        print("  ", end=" ")
+    for j in range(1,i+1):
+        print("* ",end=" ")
+    print()
+for i in reversed(range(1,n+1)):
+    for j in range(1,i):
+        print("* ", end=" ")
+    for j in range(1,n-i+1):
+        print("  ", end=" ")
+    for j in reversed(range(1,n-i+3)):
+        print("  ", end=" ")
+    for j in range(1,i):
+        print("* ",end=" ")
+    print()
