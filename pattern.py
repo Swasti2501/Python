0d3d49aa987f8d2ea7a677a280b1f850bb4959e2
Swasti Aggarwal <swastiaggarwal2501@gmail.com>
2025-11-01 17:05:10 +0530

    Print the pattern.

---
+++ pattern.py
@@ -0,0 +1,23 @@
+n = int(input("Enter the number: "))
+for i in reversed(range(1,n+1)):
+    for j in range(1,i+1):
+        print("* ",end=" ")
+    for j in range(1,n-i+1):
+        print("  ", end=" ")
+    for j in reversed(range(1,n-i+1)):
+        print("  ", end=" ")
+    for j in range(1,i+1):
+        print("* ",end=" ")
+    print()
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("* ",end=" ")
+    for j in range(1,n-i+1):
+        print("  ", end=" ")
+    for j in reversed(range(1,n-i+1)):
+        print("  ", end=" ")
+    for j in range(1,i+1):
+        print("* ",end=" ")
+    print()
+
+
